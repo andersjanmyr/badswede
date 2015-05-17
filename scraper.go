70d5130a0634663df7b8d2bcfefe1fbf85c25837
Anders Janmyr <anders@janmyr.com>
2015-05-17 11:12:37 +0200

    First page parsed to get tournaments links

--- scraper.go
+++ scraper.go
@@ -2,8 +2,11 @@ package badswede
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/headzoo/surf"
+	"github.com/headzoo/surf/browser"
 )
 
 type Query struct {
@@ -16,12 +19,34 @@ type Tournament struct {
 }
 
 func Scrape(query Query) (*Tournament, error) {
-	bow := surf.NewBrowser()
-	err := bow.Open("http://badmintonsweden.tournamentsoftware.com")
+	log.Println(query)
+	browser := surf.NewBrowser()
+	result, err := findTournaments(browser, query.Tournament)
+	if err != nil {
+		return nil, err
+	}
+	result.Each(func(_ int, s *goquery.Selection) {
+		fmt.Println(s.Attr("href"))
+	})
+	return nil, nil
+}
+
+func findTournaments(browser *browser.Browser, tournament string) (*goquery.Selection, error) {
+	err := browser.Open("http://badmintonsweden.tournamentsoftware.com")
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(bow.Title())
-	return nil, nil
+	form, err := browser.Form("#formBasePage")
+	if err != nil {
+		return nil, err
+	}
+	form.Input("tbxSearchQuery", tournament)
+	form.Input("ctl00$ctl01$cphPage$ddlSearchType", "1")
+	if err := form.Submit(); err != nil {
+		return nil, err
+	}
+	fmt.Println(browser.Title())
+	tournamentLinkSelector := "#cphPage_cphPage_tournamentlistpage_maincolumn_ctl06_ctl00_row1 h3 a"
+	selection := browser.Dom().Find(tournamentLinkSelector)
+	return selection, nil
 }
